60954713f2f25932f33dff4c8e733eaf82bd35dd
disconnectus <fka201520@gmail.com>
2024-05-24 22:05:47 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import {
+  SafeAreaView,
+} from 'react-native';
+import Header from './src/components/header';
+
+function App() {
+  return <SafeAreaView>
+    <Header/>
+  </SafeAreaView>;
+}
+
+export default App;
---
+++ src/components/header/style.js
@@ -0,0 +1,5 @@
+import { StyleSheet } from "react-native";
+
+const styleHeader = StyleSheet.create({
+    
+})
